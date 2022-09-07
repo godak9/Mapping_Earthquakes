500a64876880046bc3ba2bdf6acebe642532d16e
godak <gabyodak@Gabys-Air.attlocal.net>
2022-09-07 02:57:05 -0400

    Adding code for linestrings and formatting linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -7,8 +7,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map (this was for only one tile layer)
-//streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,17 +15,25 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+//We create a night navigation tile layer that will be an option for our map
+
+let night_nav = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 // Create a base layer that holds both maps.
 let baseMaps = {
     Light: light,
-    Dark: dark
+    Dark: dark,
+    Night: night_nav
   };
 
   // Create the map object with a center (San Francisco airport) and zoom level.
 let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 4,
-    layers: [light]
+    layers: [night_nav]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -35,17 +41,18 @@ L.control.layers(baseMaps).addTo(map);
 
 //Accessing the airport GeoJSON URL
 let torontoData = 
-"https://raw.githubusercontent.com/godak9/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json";
+"https://raw.githubusercontent.com/godak9/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
         onEachFeature: function(feature, layer){
-            layer.bindPopup("<h2> Airport Code: <b>" + feature.properties.faa + "</b> </h2> <hr> <h3> Airport Name: <b>" + feature.properties.name + "</b></h3>")
-        }
+            layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h3> Destination: " + feature.properties.dst + "</h3>")
+        },
+        color: "yellow",
+        weight: 2
     })
-    // Add a popup that displays the airports' codes and names
     .addTo(map);
 });
